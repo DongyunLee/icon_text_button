4077d65609a6d932156840e3e0f44f4b9b1acd82
ch4o5 <1150477170@qq.com>
2019-11-14 10:05:04 +0800

    init package

---
+++ lib/icon_text_button.dart
@@ -0,0 +1,17 @@
+library icon_text_button;
+
+import 'package:flutter/material.dart';
+
+class IconTextButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _IconTextButtonState();
+}
+
+class _IconTextButtonState extends State<IconTextButton>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+  
+}
