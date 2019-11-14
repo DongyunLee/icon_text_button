67e4b508571073383c943dbe8bc1d3fbb59cdf11
ch4o5 <1150477170@qq.com>
2019-11-14 17:59:28 +0800

    update example

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:icon_text_button/icon_text_button.dart';
 
 //import 'package';
 class HomePage extends StatefulWidget {
@@ -20,7 +21,34 @@ class _HomePageState extends State<HomePage> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
+	          children: <Widget>[
+		          IconTextButton(
+			          icon: Icon(Icons.school),
+			          label: Text('RaiseButton'),
+			          color: Colors.white,
+			          onPress: () {},
+		          ),
+		          IconTextButton(
+			          icon: Icon(Icons.all_inclusive),
+			          label: Text('FlatButton'),
+			          onPress: () {},
+			          color: Colors.red,
+			          btnType: BtnType.flat,
+		          ),
+		          IconTextButton(
+			          icon: Icon(Icons.ac_unit),
+			          label: Text('OutlineButton'),
+			          btnType: BtnType.outline,
+			          color: Colors.cyan,
+			          onPress: () {},
+		          ),
+		          IconTextButton(
+			          icon: Icon(Icons.import_contacts),
+			          label: Text('IconButton'),
+			          btnType: BtnType.icon,
+			          onPress: () {},
+		          )
+	          ],
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
         );
