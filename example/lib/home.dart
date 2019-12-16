be1e7d4f95f6627cb0e85675018f9353dde73c80
ch4o5 <dongyunli619@gmail.com>
2019-12-16 16:42:15 +0800

    update to use padding api
    Signed-off-by: ch4o5 <1150477170@qq.com>

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,12 +1,12 @@
+import 'package:example/icon_text_button.dart';
 import 'package:flutter/material.dart';
-import 'package:icon_text_button/icon_text_button.dart';
 
 //import 'package';
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
-
+  
   final String title;
-
+  
   @override
   _HomePageState createState() => _HomePageState();
 }
@@ -21,36 +21,40 @@ class _HomePageState extends State<HomePage> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-	          children: <Widget>[
-		          IconTextButton(
-			          icon: Icon(Icons.school),
-			          label: Text('RaiseButton'),
-			          color: Colors.white,
-			          onPress: () {},
-		          ),
-		          IconTextButton(
-			          icon: Icon(Icons.all_inclusive),
-			          label: Text('FlatButton'),
-			          onPress: () {},
-			          color: Colors.red,
-			          btnType: BtnType.flat,
-		          ),
-		          IconTextButton(
-			          icon: Icon(Icons.ac_unit),
-			          label: Text('OutlineButton'),
-			          btnType: BtnType.outline,
-			          color: Colors.cyan,
-			          onPress: () {},
-		          ),
-		          IconTextButton(
-			          icon: Icon(Icons.import_contacts),
-			          label: Text('IconButton'),
-			          btnType: BtnType.icon,
-			          onPress: () {},
-		          )
-	          ],
+            children: <Widget>[
+              IconTextButton(
+                icon: Icon(Icons.school),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                label: Text('RaiseButton'),
+                color: Colors.white,
+                onPress: () {},
+              ),
+              IconTextButton(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                icon: Icon(Icons.all_inclusive),
+                label: Text('FlatButton'),
+                onPress: () {},
+                color: Colors.red,
+                btnType: BtnType.flat,
+              ),
+              IconTextButton(
+                icon: Icon(Icons.ac_unit),
+                label: Text('OutlineButton'),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                btnType: BtnType.outline,
+                color: Colors.cyan,
+                onPress: () {},
+              ),
+              IconTextButton(
+                icon: Icon(Icons.import_contacts),
+                label: Text('IconButton'),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                btnType: BtnType.icon,
+                onPress: () {},
+              )
+            ],
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
-        );
+    );
   }
 }
--- lib/icon_text_button.dart
+++ lib/icon_text_button.dart
@@ -13,64 +13,78 @@ enum BtnType { raise, outline, flat, icon }
 /// 图文按钮
 ///
 class IconTextButton extends StatefulWidget {
-  ///
-  /// The button style that you will use
-  /// 你要使用的按钮组件样式
-  ///
-  final BtnType btnType;
-
-  ///
-  /// The icon you want to display
-  /// icon图标
-  ///
-  final Icon icon;
-
-  ///
-  /// The text you will display under the icon
-  /// 图标下的文字
-  ///
-  final Text label;
-
-  ///
-  /// The size of whole button
-  /// 大小
-  ///
-  final double size;
-
-  ///
-  /// The color of icon & text
-  /// 颜色
-  ///
-  final Color color;
-
-  ///
-  /// The color of Background
-  /// 背景颜色
-  ///
-  final Color bgColor;
-
-  ///
-  /// The event when you click our button
-  /// 点击事件
-  ///
-  final VoidCallback onPress;
-
-  final Color borderColor;
-
-  const IconTextButton(
-      {Key key,
-      @required this.icon,
-      @required this.label,
-      this.size,
-      this.color,
-      this.bgColor,
-      this.btnType,
-      @required this.onPress,
-      this.borderColor})
-      : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _IconTextButtonState();
+	///
+	/// If you need a responsive size to fit different screen resolution
+	/// If true， all size of this widget will be convert into percentage of the screen that under safe-area
+	/// 是否要使用自适应的尺寸，如果此项为true，则所有的尺寸都会转化为
+	///
+	final bool isResponsive;
+	
+	///
+	/// The button style that you will use
+	/// 你要使用的按钮组件样式
+	///
+	final BtnType btnType;
+	
+	///
+	/// The icon you want to display
+	/// icon图标
+	///
+	final Icon icon;
+	
+	///
+	/// The text you will display under the icon
+	/// 图标下的文字
+	///
+	final Text label;
+	
+	///
+	/// The size of whole button
+	/// 大小
+	///
+	final double size;
+	
+	///
+	/// The Padding around icon & text
+	/// 内边距
+	///
+	final EdgeInsetsGeometry padding;
+	
+	///
+	/// The color of icon & text
+	/// 颜色
+	///
+	final Color color;
+	
+	///
+	/// The color of Background
+	/// 背景颜色
+	///
+	final Color bgColor;
+	
+	///
+	/// The event when you click our button
+	/// 点击事件
+	///
+	final VoidCallback onPress;
+	
+	final Color borderColor;
+	
+	const IconTextButton({Key key,
+		@required this.icon,
+		@required this.label,
+		this.size,
+		this.color,
+		this.bgColor,
+		this.btnType,
+		@required this.onPress,
+		this.borderColor,
+		this.padding,
+		this.isResponsive = true})
+			: super(key: key);
+	
+	@override
+	State<StatefulWidget> createState() => _IconTextButtonState();
 }
 
 ///
@@ -78,78 +92,81 @@ class IconTextButton extends StatefulWidget {
 /// 状态类
 ///
 class _IconTextButtonState extends State<IconTextButton> {
-  @override
-  Widget build(BuildContext context) {
-    ///
-    /// The detail of our button
-    /// 按钮的内部实现
-    ///
-    var wid = Padding(
-      padding: new EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: <Widget>[
-          widget.icon,
-          const SizedBox(height: 8.0),
-          widget.label
-        ],
-      ),
-    );
-
-    ///
-    /// The switcher of button type
-    /// 按钮类型处理
-    ///
-    switch (widget.btnType) {
-      case BtnType.outline:
-        return Ink(
-          decoration: ShapeDecoration(
-            color: widget.bgColor,
-            shape: Border(),
-          ),
-          child: OutlineButton(
-            child: wid,
-            splashColor: widget.color,
-            textColor: widget.color,
-            borderSide: widget.borderColor != null
-                ? BorderSide(color: widget.borderColor)
-                : null,
-            onPressed: widget.onPress,
-          ),
-        );
-      case BtnType.flat:
-        return FlatButton(
-          child: wid,
-          textColor: widget.color,
-          splashColor: widget.color,
-          color: widget.bgColor,
-          onPressed: widget.onPress,
-        );
-      case BtnType.icon:
-        return Container(
-          child: Ink(
-            decoration: ShapeDecoration(
-              color: widget.bgColor,
-              shape: Border(),
-            ),
-            child: IconButton(
-              icon: widget.icon,
-              color: widget.color,
-              splashColor: widget.color,
-              tooltip: widget.label.data,
-              onPressed: widget.onPress,
-            ),
-          ),
-        );
-      default:
-        return RaisedButton(
-          child: wid,
-          textColor: widget.color,
-          splashColor: widget.color,
-          color: widget.bgColor,
-          onPressed: widget.onPress,
-        );
-    }
-  }
+	@override
+	Widget build(BuildContext context) {
+		
+		///
+		/// The detail of our button
+		/// 按钮的内部实现
+		///
+		var wid = Padding(
+			padding: widget.padding == null
+					? widget.padding
+					: new EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
+			child: Column(
+				mainAxisAlignment: MainAxisAlignment.center,
+				mainAxisSize: MainAxisSize.max,
+				children: <Widget>[
+					widget.icon,
+					const SizedBox(height: 8.0),
+					widget.label
+				],
+			),
+		);
+		
+		///
+		/// The switcher of button type
+		/// 按钮类型处理
+		///
+		switch (widget.btnType) {
+			case BtnType.outline:
+				return Ink(
+					decoration: ShapeDecoration(
+						color: widget.bgColor,
+						shape: Border(),
+					),
+					child: OutlineButton(
+						child: wid,
+						splashColor: widget.color,
+						textColor: widget.color,
+						borderSide: widget.borderColor != null
+								? BorderSide(color: widget.borderColor)
+								: null,
+						onPressed: widget.onPress,
+					),
+				);
+			case BtnType.flat:
+				return FlatButton(
+					child: wid,
+					textColor: widget.color,
+					splashColor: widget.color,
+					color: widget.bgColor,
+					onPressed: widget.onPress,
+				);
+			case BtnType.icon:
+				return Container(
+					child: Ink(
+						decoration: ShapeDecoration(
+							color: widget.bgColor,
+							shape: Border(),
+						),
+						child: IconButton(
+							icon: widget.icon,
+							color: widget.color,
+							splashColor: widget.color,
+							tooltip: widget.label.data,
+							onPressed: widget.onPress,
+						),
+					),
+				);
+			default:
+				return RaisedButton(
+					child: wid,
+					textColor: widget.color,
+					splashColor: widget.color,
+					color: widget.bgColor,
+					onPressed: widget.onPress,
+				);
+		}
+	}
 }
